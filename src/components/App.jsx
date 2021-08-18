1d79f767996dd84a85b9073a39742b321fa74f0d
Marius Žakevičius <m.zakevicius@gmail.com>
2021-08-18 17:05:06 +0300

    Styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,10 @@ import "../style/index.scss";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Main from "./Main.jsx";
-import Logo from "./elements/Logo.jsx";
 
 const App = () => {
   return (
     <>
-      {/* <Logo /> */}
       <Header />
       <Main />
       <Footer />
--- src/components/elements/Contacts.jsx
+++ src/components/elements/Contacts.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Worker from './Worker.jsx';
 import workers from './workers.json';
-import CompanyInfo from "./CompanyInfo.jsx"
+import CompanyInfo from "./CompanyInfo.jsx";
+import Logo from "./Logo.jsx";
 
 const Contacts = () => {  
   return (
     <section className='contacts' id='contacts'>
       <h3><span className='red'>K</span>ontaktai</h3>
       <div className="workers">
+        <Logo />
         {workers.map(worker => <Worker key={worker.name} worker={worker} />)}
       </div>
       <CompanyInfo />
