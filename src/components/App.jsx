7dc99cacb201b8ce200285c9c808788b18785917
Marius <m.zakevicius@gmail.com>
2021-08-24 18:42:07 +0300

    Language switcher

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import "../style/index.scss";
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Main from "./Main.jsx";
+import React from 'react';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import Main from './Main.jsx';
+import LanguageSwitcher from './LanguageSwitcher.jsx';
+import '../style/index.scss';
 
 const App = () => {
   return (
     <>
+      <LanguageSwitcher />
       <Header />
       <Main />
       <Footer />
