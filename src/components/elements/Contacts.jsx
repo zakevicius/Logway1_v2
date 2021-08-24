c6b42072c776ea722b4a0e1b6041b39de74348e1
Marius <m.zakevicius@gmail.com>
2021-08-24 18:47:37 +0300

    Added missing translation

--- src/components/elements/Contacts.jsx
+++ src/components/elements/Contacts.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import { withTranslation } from 'react-i18next';
 import Worker from './Worker.jsx';
 import workers from './workers.json';
-import CompanyInfo from "./CompanyInfo.jsx";
-import Logo from "./Logo.jsx";
+import CompanyInfo from './CompanyInfo.jsx';
+import Logo from './Logo.jsx';
 
-const Contacts = () => {  
+const Contacts = ({ t }) => {
   return (
     <section className='contacts' id='contacts'>
-      <h3><span className='red'>K</span>ontaktai</h3>
-      <div className="workers">
+      <h3>
+        <span className='red'>{t('contacts.title')[0]}</span>
+        {t('contacts.title').slice(1)}
+      </h3>
+      <div className='workers'>
         <Logo />
-        {workers.map(worker => <Worker key={worker.name} worker={worker} />)}
+        {workers.map((worker) => (
+          <Worker key={worker.name} worker={worker} />
+        ))}
       </div>
       <CompanyInfo />
     </section>
-  )
-}
+  );
+};
 
-export default Contacts;
+export default withTranslation()(Contacts);
