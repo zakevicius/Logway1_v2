ab99383816c4ed2612877a7205fc67e32f91f7e6
Marius <m.zakevicius@gmail.com>
2021-08-24 16:10:23 +0300

    Added react-i18next, webpack copy plugin, translations init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,56 +1,70 @@
-import React from "react";
+import React from 'react';
+import { useTranslation, withTranslation, Trans } from 'react-i18next';
 import sections from './elements/sections.json';
 
-const Header = () => {
+const Header = ({ t }) => {
+  // const { t } = useTranslation('translation');
 
   const onClick = (e) => {
-    const scrollTarget = document.getElementById(e.currentTarget.dataset.section);
+    const scrollTarget = document.getElementById(
+      e.currentTarget.dataset.section
+    );
     const xPos = scrollTarget.offsetLeft;
     const yPos = scrollTarget.offsetTop;
 
-    for (let i = 0; i <= yPos; i++) {      
+    for (let i = 0; i <= yPos; i++) {
       setTimeout(() => {
         window.scrollTo(xPos, i);
-      }, 1 / 10 * i)
+      }, (1 / 10) * i);
     }
-  }
+  };
 
   const onMouseEnter = (e) => {
     e.currentTarget.querySelector('.headerInfo-title').classList.add('active');
-  }
+  };
 
   const onMouseLeave = (e) => {
-    e.currentTarget.querySelector('.headerInfo-title').classList.remove('active');
-  }
+    e.currentTarget
+      .querySelector('.headerInfo-title')
+      .classList.remove('active');
+  };
 
-  return <header>
-    <div className='headerLogo'></div>
-    <div className='headerInfo'>
-      {sections.map(item => (
+  return (
+    <header>
+      <div className='headerLogo'></div>
+      <div className='headerInfo'>
+        {sections.map((item) => (
+          <div
+            data-section={item.section}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            onClick={onClick}
+            key={item.section}
+            className={`${item.section} header-item`}
+          >
+            <div className='background'></div>
+            <h2 className='headerInfo-title'>
+              {t('title')}
+              <span className='red'>{item.title[0]}</span>
+              {item.title.slice(1)}
+            </h2>
+          </div>
+        ))}
         <div
-          data-section={item.section} 
-          onMouseEnter={onMouseEnter} 
+          data-section='contacts'
+          onMouseEnter={onMouseEnter}
           onMouseLeave={onMouseLeave}
-          onClick={onClick} 
-          key={item.section} 
-          className={`${item.section} header-item`}
+          onClick={onClick}
+          className='contacts header-item'
         >
           <div className='background'></div>
-          <h2 className='headerInfo-title'><span className='red'>{item.title[0]}</span>{item.title.slice(1)}</h2>
+          <h2 className='headerInfo-title'>
+            <span className='red'>K</span>ontaktai
+          </h2>
         </div>
-      ))}
-      <div 
-        data-section="contacts" 
-        onMouseEnter={onMouseEnter} 
-        onMouseLeave={onMouseLeave}
-        onClick={onClick} 
-        className='contacts header-item'
-      >
-        <div className='background'></div>  
-        <h2 className='headerInfo-title'><span className='red'>K</span>ontaktai</h2>
       </div>
-    </div>
-  </header>;
+    </header>
+  );
 };
 
-export default Header;
+export default withTranslation()(Header);
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,54 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   entry: {
-    main: './src/index.jsx'
+    main: './src/index.jsx',
   },
+  plugins: [
+    new CopyPlugin(
+      {
+        patterns: [
+          {
+            from: './src/locales',
+            to: '../dist/locales',
+            toType: 'dir',
+            force: true,
+          },
+        ],
+      },
+      { copyUnmodified: true }
+    ),
+  ],
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node-modules/,
-        use: 'babel-loader'
+        use: 'babel-loader',
       },
       {
         test: /\.jsx$/,
         exclude: /node-modules/,
-        use: 'babel-loader'
+        use: 'babel-loader',
       },
       {
         test: /\.(svg|png|jpeg|jpg|gif)$/,
         use: {
           loader: 'file-loader',
           options: {
-            name: "[name].[hash].[ext]",
-            outputPath: "assets/img"
-          }
-        }
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets/img',
+          },
+        },
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]',
-          outputPath: 'assets/fonts'
-        }
-      }
-    ]
-  }
-}
+          outputPath: 'assets/fonts',
+        },
+      },
+    ],
+  },
+};
